48b084d6f6a16f8a6e10d7ee284a56cc8097225d
Build <build@loanhelper.app>
2026-05-28 20:36:44 +0800

    fix: PhotoPicker delegate uses NSObject subclass

--- modules/photo-picker/ios/PhotoPickerModule.swift
+++ modules/photo-picker/ios/PhotoPickerModule.swift
@@ -1,10 +1,7 @@
-﻿import ExpoModulesCore
+import ExpoModulesCore
 import UIKit
 
 public class PhotoPickerModule: Module {
-  private var promise: Promise?
-  private var picker: UIImagePickerController?
-
   public func definition() -> ModuleDefinition {
     Name("PhotoPicker")
 
@@ -13,17 +10,15 @@ public class PhotoPickerModule: Module {
     }
 
     AsyncFunction("pickFromLibrary") { (promise: Promise) in
-      self.promise = promise
-
       DispatchQueue.main.async {
+        let helper = PhotoPickerHelper(promise: promise)
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary
         picker.mediaTypes = ["public.image"]
-        picker.delegate = self
+        picker.delegate = helper
 
-        // Get the topmost view controller to present from
-        if let rootVC = self.getRootViewController() {
-          self.picker = picker
+        if let rootVC = PhotoPickerHelper.getRootViewController() {
+          PhotoPickerHelper.currentHelper = helper
           rootVC.present(picker, animated: true)
         } else {
           promise.reject("NO_VIEW_CONTROLLER", "Could not find root view controller")
@@ -31,8 +26,46 @@ public class PhotoPickerModule: Module {
       }
     }
   }
+}
 
-  private func getRootViewController() -> UIViewController? {
+class PhotoPickerHelper: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  let promise: Promise
+
+  init(promise: Promise) {
+    self.promise = promise
+    super.init()
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+    picker.dismiss(animated: true)
+    PhotoPickerHelper.currentHelper = nil
+
+    if let imageUrl = info[.imageURL] as? URL {
+      promise.resolve(imageUrl.absoluteString)
+    } else if let image = info[.originalImage] as? UIImage {
+      let tempDir = FileManager.default.temporaryDirectory
+      let fileName = "photo_\(Date().timeIntervalSince1970).jpg"
+      let fileUrl = tempDir.appendingPathComponent(fileName)
+      if let data = image.jpegData(compressionQuality: 0.8) {
+        try? data.write(to: fileUrl)
+        promise.resolve(fileUrl.absoluteString)
+      } else {
+        promise.reject("SAVE_ERROR", "Could not save image")
+      }
+    } else {
+      promise.reject("NO_IMAGE", "No image selected")
+    }
+  }
+
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true)
+    PhotoPickerHelper.currentHelper = nil
+    promise.reject("CANCELLED", "User cancelled")
+  }
+
+  static var currentHelper: PhotoPickerHelper?
+
+  static func getRootViewController() -> UIViewController? {
     var window: UIWindow?
     if #available(iOS 15.0, *) {
       window = UIApplication.shared.connectedScenes
@@ -47,38 +80,6 @@ public class PhotoPickerModule: Module {
       window = UIApplication.shared.windows.first { $0.isKeyWindow }
     }
     return window?.rootViewController?.topMostViewController()
-  }
-}
-
-extension PhotoPickerModule: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-    picker.dismiss(animated: true)
-
-    if let imageUrl = info[.imageURL] as? URL {
-      self.promise?.resolve(imageUrl.absoluteString)
-    } else if let image = info[.originalImage] as? UIImage {
-      // Save to temp directory and return path
-      let tempDir = FileManager.default.temporaryDirectory
-      let fileName = "photo_\(Date().timeIntervalSince1970).jpg"
-      let fileUrl = tempDir.appendingPathComponent(fileName)
-      if let data = image.jpegData(compressionQuality: 0.8) {
-        try? data.write(to: fileUrl)
-        self.promise?.resolve(fileUrl.absoluteString)
-      } else {
-        self.promise?.reject("SAVE_ERROR", "Could not save image")
-      }
-    } else {
-      self.promise?.reject("NO_IMAGE", "No image selected")
-    }
-    self.picker = nil
-    self.promise = nil
-  }
-
-  public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-    picker.dismiss(animated: true)
-    self.promise?.reject("CANCELLED", "User cancelled")
-    self.picker = nil
-    self.promise = nil
   }
 }
 
